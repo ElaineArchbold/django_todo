c195fa2f5d53e22ce14d1bee84c214c7f187b6bf
Elaine Archbold <e.t.archbold@gmail.com>
2020-03-11 13:20:22 +0000

    update allowed users

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5m)c^i*d*wt6ntd^9fwuj5glwvrwc+0*9erq$omwplf7x)h89^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['my-simple-django-todo.herokuapp.com/']
+ALLOWED_HOSTS = ['my-simple-django-todo.herokuapp.com']
 
 
 # Application definition
